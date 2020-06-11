6ecaa62b519929acae4d96edd8c4629ac770a857
Arthur Kalikiti <arthur@kalikiti.net>
2020-06-11 07:05:50 +0200

    Added seat depletion

--- AirlineReservation.java
+++ AirlineReservation.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AirlineReservation {
+class AirlineReservation {
     static int[] seats = new int[20];
-    public static void main(String args[]) {
+    static ArrayList<Integer> occupied = new ArrayList<>();
+    public static void main(String[] args) {
         System.out.println("Type 1 for first class or 2 for economy class");
         Scanner input = new Scanner(System.in);
         int flightClass = input.nextInt();
@@ -21,20 +22,33 @@ public class AirlineReservation {
     }
 
     private static int firstClass() {
-        ArrayList<Integer> occupied = new ArrayList<>();
+        Boolean terminate = false;
         Scanner input = new Scanner(System.in);
-        for (int i = 1; i <= 8; i++) {
-            if (seats[i] == 0) {
-                occupied.add(i);
-            }
+        
+        while (!terminate) {
+            availableSeats();
             System.out.println("Please choose a seat from the available ones "+ occupied);
             int seatNumber = input.nextInt();
             seats[seatNumber] = 1;
+            if (seatNumber > 8) {
+                System.out.println("That seat number is only available in economy");
+                terminate = true;
+            } 
         }
         return 0;
     }
 
     private static int economyClass() {
+        return 0;
+    }
+
+    private static int availableSeats() {
+        occupied.clear();
+        for (int i = 1; i <= 8; i++) {
+            if (seats[i] == 0) {
+                occupied.add(i);
+            }
+        }
         return 0;
     }
 }
